36e3c34b09f95f71f234c058176f42326b954daa
raqia <abdiraqia5@gamil.com>
2025-02-01 18:59:37 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,18 +10,6 @@ module.exports = {
         secondary: "#51344c",
         accent: "#784f5d",
       },
-      screens: {
-        // mobile: '400px',
-        // => @media (min-width: 640px) { ... }
-        // tablet: '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        // laptop: '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        // desktop: '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
       
     }
     
